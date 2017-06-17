922b63f875a56e78d0ee1cd4dffdcc61469a6ff2
Je <git@iamje.com>
2017-06-17 20:47:27 +0800

    add less support

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         }
       },
       {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader'
+      },
+      {
         test: /\.(png|jpe?g|gif|svg|ttf|otf|woff2?)(\?.*)?$/,
         loader: 'file-loader',
         options: {
