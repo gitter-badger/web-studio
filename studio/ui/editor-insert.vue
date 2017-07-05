93c586e4c73f6b600f20f551cd0e7983d01bb2ef
Je <git@iamje.com>
2017-07-06 02:10:24 +0800

    add editor insert component

---
+++ studio/ui/editor-insert.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="editor-insert">
+        <div class="mask" @click="collapsed=false"></div>
+        <ul>
+            <li>
+            </li>
+        </ul>
+        <div class="add" @click="collapsed=!collapsed"></div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue, { ComponentOptions } from 'vue'
+
+    interface InsertComponent extends Vue {
+        collapsed: boolean
+    }
+
+    export default {
+        data() {
+            return {
+                collapsed: false
+            }
+        }
+    } as ComponentOptions<InsertComponent>
+</script>
+
+<style lang="less">
+    #editor-insert {
+        width: auto;
+    }
+</style>
