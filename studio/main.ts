72443cfe503fcd027f65a0a2b2844e8b71d65e84
Je <git@iamje.com>
2017-07-09 19:13:30 +0800

    remove global store

--- studio/main.ts
+++ studio/main.ts
@@ -1,75 +1,10 @@
 import url from 'url'
 import Vue from 'vue'
-import { ipcRenderer } from 'electron'
 import editor from './editor'
 import welcome from './welcome'
 import preferences from './preferences'
 import './assets/ui.less'
 import './assets/elements.less'
-
-interface AppState extends Vue {
-  isMac: boolean,
-  windowWidth: number,
-  windowHeight: number,
-  leftAsideWidth: number,
-  showLayers: boolean,
-  showInspector: boolean,
-  previewMode: boolean,
-}
-
-const initState = ipcRenderer.sendSync('project-property', ['leftAsideWidth', 'showLayers', 'showInspector', 'previewMode']) as AppState
-const $state = new Vue({
-  data: {
-    isMac: process.platform === 'darwin',
-    windowWidth: window.innerWidth,
-    windowHeight: window.innerHeight,
-    leftAsideWidth: initState.leftAsideWidth,
-    showLayers: initState.showLayers,
-    showInspector: initState.showInspector,
-    previewMode: initState.previewMode,
-  },
-}) as AppState
-
-ipcRenderer.on('project-property', (e: Event, key: string, value: any) => {
-  switch (key) {
-    case 'showLayers':
-    case 'showInspector':
-    case 'previewMode':
-      $state[key] = value as boolean
-    default:
-  }
-})
-
-window.addEventListener('resize', () => {
-  $state.windowWidth = window.innerWidth
-  $state.windowHeight = window.innerHeight
-})
-
-Vue.mixin({
-  computed: {
-    isMac: () => $state.isMac,
-    windowWidth: () => $state.windowWidth,
-    windowHeight: () => $state.windowHeight,
-    leftAsideWidth: () => $state.leftAsideWidth,
-    showLayers: () => $state.showLayers,
-    showInspector: () => $state.showInspector,
-    previewMode: () => $state.previewMode,
-  },
-  methods: {
-    $alterLeftAsideWidth: (n: number) => {
-      let width = $state.leftAsideWidth + n
-
-      if (width < 180) {
-        width = 180
-      } else if (width > $state.windowWidth / 2) {
-        width = $state.windowWidth / 2
-      }
-
-      ipcRenderer.send('app-storage', 'leftAsideWidth', width)
-      $state.leftAsideWidth = width
-    },
-  },
-})
 
 let appRoot = new Vue({
   el: '#app',
