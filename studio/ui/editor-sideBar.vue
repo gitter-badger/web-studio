fe1a77f6e4f54dacf2975934598c36e73b5bc975
Je <git@iamje.com>
2017-07-20 20:08:39 +0800

    complete the 'edit-layer-add' and 'edit-layer-trees' ui

--- studio/ui/editor-sideBar.vue
+++ studio/ui/editor-sideBar.vue
@@ -1,25 +1,33 @@
 <template>
     <div :style="{width: sideBarWidth+'px'}" id="editor-sidebar">
         <div id="sidebar-topfill" v-show="!fullscreen"></div>
-        <div id="press-button" class="icon-press" @click="$emit('press')" title="Press" v-show="!fullscreen"></div>
+        <div id="press-button" class="icon-press" @click="$press()" title="Press" v-show="!fullscreen"></div>
         <div id="project-title" :style="{top: (fullscreen ? 18 : 48) + 'px'}">
             <div class="icon" :style="{borderColor: '#fff'}"></div>
             <div class="title">
-                {{web.title || documentName}}
-                <span v-if="edited"> &nbsp;—&nbsp; Edited</span>
+                <span>{{web.title || documentName}}</span>
+                <em v-if="edited"> &nbsp;—&nbsp; Edited</em>
+                <em v-if="saving"> &nbsp;—&nbsp; Saving &middot;&middot;&middot;</em>
             </div>
         </div>
-        <div id="edit-layers-nav">
-            <div class="icon icon-layers" :class="{current: showLayers === 'pages'}" @click="$showLayers('pages')"></div>
-            <div class="icon icon-box" :class="{current: showLayers === 'components'}" @click="$showLayers('components')"></div>
-            <div class="icon icon-plugin" :class="{current: showLayers === 'extends'}" @click="$showLayers('extends')"></div>
+        <div id="edit-layer-tabs">
+            <div class="icon icon-layers" :class="{current: editTab === 'pages'}" @click="$setEditTab('pages')" title="Pages"></div>
+            <div class="icon icon-box" :class="{current: editTab === 'components'}" @click="$setEditTab('components')" title="Components"></div>
+            <div class="icon icon-plugin" :class="{current: editTab === 'extensions'}" @click="$setEditTab('extensions')" title="Extensions"></div>
         </div>
-        <div id="edit-layers" :style="{width: sideBarWidth - 90 + 'px', top: (fullscreen ? 18 : 48) + 'px'}">
-            <div class="layer-tree" v-for="layer in web[showLayers]" :key="layer">
-                <span class="icon" :class="'icon-arrow-' + (layer.collapsed ? 'collapse' : 'expand')" @click="$noRecordUpdateWeb(layer, 'collapsed', !layer.collapsed)"></span>
-                <span class="title">{{layer.title}}</span>
-                <div class="actions"></div>
-            </div>
+        <div id="edit-layer-add" @click="$addWebObject">
+            <span class="icon icon-plus-small"></span>
+            <span class="text">New</span>
+            <span class="text" v-if="editTab === 'pages'">Page</span>
+            <span class="text" v-if="editTab === 'components'">Component</span>
+            <span class="text" v-if="editTab === 'extensions'">Extension</span>
+        </div>
+        <div id="edit-layer-trees" :style="{width: sideBarWidth - 75 + 'px', height: windowHeight - (fullscreen ? 18 : 48+30) - 18 + 'px', top: (fullscreen ? 18 : 48+30) + 'px'}">
+            <layerTree :layer="webObject" :root="webObject" :status-text="editTab === 'pages' ? webObject.routeExp : null" v-for="webObject in web[editTab]" :key="webObject.id">
+                <span class="icon icon-input"></span>
+                <span class="icon icon-settings"></span>
+                <span class="icon icon-path"></span>
+            </layerTree>
         </div>
     </div>
 </template>
@@ -36,7 +44,7 @@
             return {
 
             }
-        }
+        },
     } as ComponentOptions<Vue>
 </script>
 
@@ -48,7 +56,6 @@
         height: 100%;
         background-color: #212121;
         overflow: hidden;
-        overflow-y: auto;
     }
 
     #sidebar-topfill {
@@ -92,13 +99,14 @@
             writing-mode: vertical-rl;
             color: #ccc;
 
-            span {
+            em {
+                font-style: normal;
                 color: #666;
             }
         }
     }
 
-    #edit-layers-nav {
+    #edit-layer-tabs {
         position: absolute;
         bottom: 18px;
         left: 18+2px;
@@ -121,46 +129,36 @@
         }
     }
 
-    #edit-layers {
-        position: absolute;
+    #edit-layer-add {
+       position: absolute;
         top: 48px;
         left: 75px;
+        width: 100%;
+        height: 30px;
+        cursor: pointer;
+        color: rgba(255, 255, 255, 0.6);
 
-        .layer-tree {
-            margin-bottom: 3px;
-
-            .header {
-                position: relative;
-                width: 100%;
-
-                .icon {
-                    font-size: 16px;
-                    line-height: 16px;
-                    vertical-align: middle;
-                    color: rgba(255, 255, 255, 0.6);
-                    transition: color 0.24s;
-                    cursor: pointer;
-                }
-
-                .text {
-                    font-size: 12px;
-                    line-height: 16px;
-                    vertical-align: middle;
-                    text-transform: uppercase;
-                }
-
-                .title {
-                    color: rgba(255, 255, 255, 0.6);
-                    transition: color 0.24s;
-
-
-
-                    &:hover,
-                    &.current {
-                        color: #fff;
-                    }
-                }
-            }
+        &:hover {
+            color: #f7f7f7;
         }
+
+        .icon {
+            font-size: 16px;
+            line-height: 16px;
+            vertical-align: middle;
+            transition: color 0.24s;
+        }
+
+        .text {
+            font-size: 12px;
+            transition: color 0.24s;
+        }
+    }
+
+    #edit-layer-trees {
+        position: absolute;
+        left: 75px;
+        overflow: hidden;
+        overflow-y: auto;
     }
 </style>
