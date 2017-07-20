05bad5fceaef71c88d81b56917ef3068f666da19
Je <git@iamje.com>
2017-07-20 20:07:42 +0800

    add rename and delete popup menu

--- studio/ui/editor-layerTree.vue
+++ studio/ui/editor-layerTree.vue
@@ -1,47 +1,114 @@
 <template>
-	<div class="layer-tree" :class="{selected: layer.selected}" @click="select" @dblclick="select">
-		<span class="icon" :class="'icon-arrow-' + (layer.collapsed ? 'collapse' : 'expand')" @click.stop="$noRecordUpdateWeb(layer, 'collapsed', !layer.collapsed)" v-if="layer.type === 'group'"></span>
-		<span class="icon" :class="'icon-element-' + layer.type"></span>
-		<span class="title-edit" v-if="input">
-			<input v-model="layer.title" @focus="input = false" @keypress.enter="input = false">
+	<div class="layer-tree" :id="layer.id" :class="{selected: layer.selected}" @click="onclick" @contextmenu="$showPopupMenu(localPopupMenu)" v-if="layer">
+		<span class="icon" :class="'icon-arrow-' + (layer.collapsed ? 'collapse' : 'expand')" @click.stop="$setQuietly(layer, 'collapsed', !layer.collapsed)" v-if="layer.type === 'group' || layer === root"></span>
+		<span class="icon" :class="'icon-element-' + layer.type" v-if="layer.type"></span>
+		<span class="name-edit" v-if="input">
+			<input v-model.trim="inputValue" @blur.once="endRename" @keyup.enter="endRename" ref="input">
 		</span>
-		<span class="title" @dblclick.stop="rename" v-else>{{layer.title}}</span>
-		<div class="status" :class="{locked: layer.locked, hidden: layer.hidden}">
-			<span class="icon icon-lock" @click.stop="$set(layer, 'locked', !layer.locked)"></span>
-			<span class="icon icon-hidden" @click.stop="$set(layer, 'hidden', !layer.hidden)"></span>
+		<span class="name" @dblclick.prevent="ondblclick" @click.stop="onclick" v-else>{{layer.name}}</span>
+		<div class="actions" :class="{locked: layer.locked, hidden: layer.hidden}">
+			<span class="status-text" v-if="statusText">{{statusText}}</span>
+			<slot>
+				<span class="icon icon-lock" @click.stop="$setQuietly(layer, 'locked', !layer.locked)"></span>
+				<span class="icon icon-hidden" @click.stop="$setQuietly(layer, 'hidden', !layer.hidden)"></span>
+			</slot>
 		</div>
-		<layer-tree :layer="l" :parent="layer" v-for="l in layer.children" :key="l"></layer-tree>
+		<layer-tree :layer="l" :root="root" v-for="l in layer.children" :key="l.id"></layer-tree>
 	</div>
 </template>
 
 <script lang="ts">
-	import Vue, { ComponentOptions } from 'vue'
+	import Vue, { ComponentOptions, PropOptions } from 'vue'
 
 	interface LayerComponent extends Vue, EditorScope {
 		layer: any
+		root: any
 		input: boolean
+		inputValue: any
+		popupMenu: any[]
+		focus(): void
+		rename(): void
 	}
 
 	export default {
 		name: 'layer-tree',
-		props: ['layer', 'parent'],
+		props: {
+			layer: { type: Object, required: true },
+			root: { type: Object, required: true },
+			popupMenu: { type: Array, default: () => [] },
+			statusText: String,
+		},
 		data() {
 			return {
-				input: false
+				input: false,
+				inputValue: null,
+			}
+		},
+		computed: {
+			localPopupMenu(): any[] {
+				return [
+					{
+						label: 'Rename',
+						callback: 'rename'
+					},
+					{
+						label: 'Delete',
+						callback: 'delete'
+					}
+				].concat(this.popupMenu)
 			}
 		},
 		methods: {
-			select: function (e) {
+			onclick(e): void {
+				if (this.input) {
+					return
+				}
+
+				if (this.layer === this.root) {
+					this.$setQuietly(this.layer, 'collapsed', !this.layer.collapsed)
+					return
+				}
+
+				if (this.layer.selected) {
+					return
+				}
+
 				if (!this.layer.selected) {
-					Vue.set(this.layer, 'selected', true)
+					this.$setQuietly(this.layer, 'selected', true)
 				}
 			},
-			rename: function () {
-				if (!this.layer.selected) {
-					this.$setSelections(this.layer)
+			ondblclick(e): void {
+				// root can be renamed by double click
+				if (this.layer === this.root) {
+					return
+				}
+
+				this.rename()
+			},
+			focus(): void {
+				if (this.$refs.input) {
+					(this.$refs.input as HTMLInputElement).select()
+				}
+			},
+			rename(): void {
+				if (!this.layer.selected && this.layer !== this.root) {
+					this.$setQuietly(this.layer, 'selected', true)
+					this.$setSelections(this.layer, this.root)
 				}
 
 				this.input = true
+				this.inputValue = this.layer.name
+				this.$nextTick(() => this.focus())
+			},
+			endRename(): void {
+				if (this.inputValue) {
+					this.layer.name = this.inputValue
+				}
+				this.input = false
+				this.inputValue = null
+			},
+			delete(): void {
+				this.$removeWebObject(this.root)
 			}
 		},
 	} as ComponentOptions<LayerComponent>
@@ -52,6 +119,7 @@
 		position: relative;
 		width: 100%;
 		margin-bottom: 3px;
+		cursor: pointer;
 
 		.icon {
 			font-size: 16px;
@@ -59,22 +127,17 @@
 			vertical-align: middle;
 			color: rgba(255, 255, 255, 0.6);
 			transition: color 0.24s;
-			cursor: pointer;
 		}
 
-		.text {
+		.name {
 			font-size: 12px;
-			line-height: 16px;
-			vertical-align: middle;
-			text-transform: uppercase;
-		}
-
-		.title {
 			color: rgba(255, 255, 255, 0.6);
 			transition: color 0.24s;
+		}
 
-			&:hover,
-			&.current {
+		&:hover,
+		&.current {
+			.name {
 				color: #fff;
 			}
 		}
